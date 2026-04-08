8ad93961e3f6146785dd4f89570ff3bd754f977b
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-08 13:35:52 +0300

    Add enemy spawn

--- Assets/Game/Scripts/Enemy/SimpleEnemyFactory.cs
+++ Assets/Game/Scripts/Enemy/SimpleEnemyFactory.cs
@@ -2,17 +2,18 @@
 
 namespace Assets.Game.Scripts.Enemy
 {
+    [CreateAssetMenu(fileName = "Simple_enemy_factory", menuName = "Enemies/Simple enemy factory")]
     public class SimpleEnemyFactory : EnemyFactory
     {
         public int Damage = 2;
         
         [SerializeField] private SimpleEnemy _prefab;
 
-        public override Enemy Create()
+        public override Enemy Create(Health target)
         {
             var simpleEnemy = Instantiate(_prefab);
 
-            simpleEnemy.Init(null, config: this);
+            simpleEnemy.Init(target, config: this);
 
             return simpleEnemy;
         }
--- Assets/Game/Scripts/GameplayMain.cs
+++ Assets/Game/Scripts/GameplayMain.cs
@@ -4,9 +4,11 @@ namespace Assets.Game.Scripts
 {
     public class GameplayMain : MonoBehaviour
     {
+        [SerializeField] private EnemyWavesController _enemyWavesController;
+
         private void Start()
         {
-            
+            _enemyWavesController.Init();
         }
     }
 }
