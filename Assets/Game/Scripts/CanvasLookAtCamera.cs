f308e01c77ea0e588d2adbfb7f82da9eb9730698
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-08 21:31:02 +0300

    Add enemy health bar

---
+++ Assets/Game/Scripts/CanvasLookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Scripts
+{
+    public class CanvasLookAtCamera : MonoBehaviour
+    {
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            if (mainCamera == null) return;
+
+            transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+                 mainCamera.transform.rotation * Vector3.up);
+        }
+    }
+}
