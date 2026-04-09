7e627ffd7953da80adc6f5b003181dd4e55e5161
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-09 20:08:06 +0300

    Add input

---
+++ Assets/Game/Scripts/Input/GameInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Game.Scripts.Input
+{
+    public class GameInput : IDisposable
+    {
+        public event Action<Vector2> Tap;
+
+        private InputActions _inputActions;
+
+        public GameInput()
+        {
+            _inputActions = new();
+
+            _inputActions.Gameplay.Enable();
+
+            _inputActions.Gameplay.Tap.performed += OnTapPerformedHandler;
+        }
+
+        private void OnTapPerformedHandler(InputAction.CallbackContext context)
+        {
+            Tap?.Invoke(_inputActions.Gameplay.TapPosition.ReadValue<Vector2>());
+        }
+
+        public void Dispose()
+        {
+            _inputActions.Gameplay.Disable();
+            _inputActions.Dispose();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Services/BuildingController.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Game.Scripts.Input;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Game.Scripts.Services
+{
+    public class BuildingController : MonoBehaviour
+    {
+        [SerializeField] private Transform _planeCenter;
+
+        private GameInput _input;
+        private Camera _mainCamera;
+
+        [Inject]
+        private void Construct(GameInput input)
+        {
+            _input = input;
+        }
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+
+            _input.Tap += OnTapHandler;
+        }
+
+        private void OnTapHandler(Vector2 tapPosition)
+        {
+            var point = GetPoint(tapPosition);
+
+            // check distance to other buldings 
+
+
+        }
+
+        private Vector3 GetPoint(Vector2 tapPosition)
+        {
+            var ray = _mainCamera.ScreenPointToRay(tapPosition);
+
+            var buildPlane = new Plane(Vector3.up, _planeCenter.position);
+
+            if (buildPlane.Raycast(ray, out var enter))
+            {
+                return ray.GetPoint(enter);
+            }
+
+            return Vector3.zero;
+        }
+    }
+}
