ad7c4eedf7b98b43bd31f3443fd8a465019527df
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-09 13:24:18 +0300

    Add idle barrel animation

---
+++ Assets/Game/Scripts/IdleScaleAnimation.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Game.Scripts
+{
+    public class IdleScaleAnimation : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _scale = 1.1f;
+        [SerializeField] private float _duration = 1.5f;
+        [SerializeField] private Ease _ease = Ease.InSine;
+
+        private Vector3 _initialScale;
+        private Tween _tween;
+
+        private void Start()
+        {
+            _initialScale = _target.localScale;
+
+            _tween = _target.transform.DOScale(_scale, _duration)
+                .SetEase(_ease)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+
+        private void OnDestroy()
+        {
+            _tween?.Kill();
+            _tween = null;
+
+            if (_target != null)
+                _target.localScale = _initialScale;
+        }
+    }
+}
