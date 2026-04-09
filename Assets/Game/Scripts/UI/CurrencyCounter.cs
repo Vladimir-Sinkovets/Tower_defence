34c37d5e9a1ed6b545ee87caa23ec68da720c575
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-09 09:15:04 +0300

    Add currency

---
+++ Assets/Game/Scripts/UI/CurrencyCounter.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Game.Scripts.Services;
+using System;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Game.Scripts.UI
+{
+    public class CurrencyCounter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _currencyText;
+
+        [Inject]
+        private void Construct(CurrencyBank currencyBank)
+        {
+            currencyBank.OnCurrencyChanged += OnCurrencyChangedHandler;
+        }
+
+        private void OnCurrencyChangedHandler(int total)
+        {
+            _currencyText.text = $"${total}";
+        }
+    }
+}
