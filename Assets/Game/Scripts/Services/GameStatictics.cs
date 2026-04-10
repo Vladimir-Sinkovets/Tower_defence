426a8cf56e76bab48a362e8bca80a81af972e88f
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-10 17:14:09 +0300

    Add statictics

---
+++ Assets/Game/Scripts/Services/GameStatictics.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Game.Scripts.Services
+{
+    public class GameStatictics
+    {
+        private int _killedEnemiesCount;
+
+        public GameStatictics(EnemyEvents enemyEvents)
+        {
+            enemyEvents.OnEnemyDied += OnEnemyDiedHandler;
+        }
+
+        public int KilledEnemiesCount { get => _killedEnemiesCount; }
+
+        private void OnEnemyDiedHandler() => _killedEnemiesCount++;
+    }
+}
