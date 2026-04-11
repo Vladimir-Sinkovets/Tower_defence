68db11ea1363bb6f9d8b113b1cd52e90627abf11
Vladimir Sinkovets <vovasin49@gmail.com>
2026-04-11 08:37:42 +0300

    Add metacurrency counter to the menu

---
+++ Assets/Game/Scripts/UI/MetaCurrencyCounter.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Game.Scripts.Services;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Game.Scripts.UI
+{
+    public class MetaCurrencyCounter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _textField;
+
+        private MetaCurrencyService _metaCurrencyService;
+
+        [Inject]
+        private void Construct(MetaCurrencyService metaCurrencyService) => _metaCurrencyService = metaCurrencyService;
+
+        private void Start()
+        {
+            _textField.text = _metaCurrencyService.Total.ToString();
+        }
+    }
+}
